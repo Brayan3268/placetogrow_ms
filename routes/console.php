0f0cd101d1aad19f3603760d86f435565631936a
Brayan <brayanlujan.mzu@hotmail.com>
2024-09-30 21:24:01 -0500

    Feat: made automaticlly the commands for collects

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    Artisan::call('app:reduce-days-command');
+    Artisan::call('app:collect-command');
+})->everyMinute();
+
+/*Schedule::call(function () {
+    Artisan::call('app:reduce-days-command');
+    Artisan::call('app:collect-command');
+})->daily();*/
