f74342bddb9d51dbe1e6b064634575390bb46710
Brayan <brayanlujan.mzu@hotmail.com>
2024-10-16 22:14:15 -0500

    Feat: add button to redirect to deploy documentation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,10 +23,16 @@
             height: auto;
             padding: 10px 20px;
             border-radius: 10px;
-            margin-bottom: 40px;
+            margin-bottom: 20px; /* Cambié el margen inferior para un mejor espaciado */
+        }
+        .button-container {
+            display: flex;
+            justify-content: space-between; /* Distribuye los botones a los extremos */
+            width: 100%; /* Ocupa el ancho completo del contenedor */
+            max-width: 400px; /* Ajusta el ancho máximo según tu preferencia */
+            padding: 10px 20px; /* Añade espacio alrededor del contenedor */
         }
         .login-button {
-            margin-top: 20px;
             padding: 10px 20px;
             background-color: #ff7c06;
             color: white;
@@ -34,6 +40,8 @@
             border-radius: 5px;
             cursor: pointer;
             text-decoration: none;
+            flex: 1; /* Permite que los botones crezcan y ocupen espacio */
+            margin: 0 110px; /* Espacio entre los botones */
         }
         .login-button:hover {
             background-color: #702bd8;
@@ -45,7 +53,10 @@
         <h1>Welcome to PlaceToGrow Microsites manager</h1>
         <img src="{{ asset('images/welcome.jpg') }}" alt="Welcome Image" class="welcome-image">
         <br>
-        <a href="{{ route('login') }}" class="login-button">Login</a>
+        <div class="button-container">
+            <a href="https://placetogrow-docu.vercel.app/" target="_blank" class="login-button">Docu</a>
+            <a href="{{ route('login') }}" class="login-button">Login</a>
+        </div>
     </div>
 </body>
 </html>
